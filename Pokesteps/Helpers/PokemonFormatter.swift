8496804e8ae8682d82a36741f253a5d908598594
Van Truong <43657607+vtruo009@users.noreply.github.com>
2025-06-11 14:40:43 -0700

    create formatters to format height & weight

---
+++ Pokesteps/Helpers/PokemonFormatter.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonFormatter.swift
+//  Pokesteps
+//
+//  Created by Van Truong on 6/11/25.
+//
+
+import Foundation
+
+struct PokemonFormatter {
+    static func formattedHeight(_ decimeters: Int) -> String {
+        let meters = Double(decimeters) * 0.1
+        let totalInches = meters * 39.3701
+        let feet = Int(totalInches / 12)
+        let inches = Int(totalInches.truncatingRemainder(dividingBy: 12))
+        return String("\(feet)'\(inches)\"")
+    }
+    
+    static func formattedWeight(_ hectograms: Int) -> String {
+        let kg = Double(hectograms) * 0.1
+        let pounds = kg * 2.20462
+        return String(format: "%.2f lbs", pounds)
+    }
+}
